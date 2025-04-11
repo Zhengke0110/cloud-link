3a41b5f9f440c685e22b6bb5b5e902eb2e013c4f
zhengke <zhengke996@163.com>
2025-04-11 23:01:38 +0800

    feat(exception): 添加业务异常处理类和自定义异常处理器
    - 新增 BizException 类，用于处理业务异常
    - 添加 CustomExceptionHandler 类，用于全局异常捕获和处理- 优化了异常处理逻辑，提高了系统稳定性和用户体验

---
+++ cloud-common/src/main/java/fun/timu/cloud/net/exception/BizException.java
@@ -0,0 +1,25 @@
+package fun.timu.cloud.net.exception;
+
+import fun.timu.cloud.net.enums.BizCodeEnum;
+import lombok.Data;
+
+@Data
+public class BizException extends RuntimeException {
+    private int code;
+
+    private String msg;
+
+    public BizException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.msg = message;
+    }
+
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+
+}
