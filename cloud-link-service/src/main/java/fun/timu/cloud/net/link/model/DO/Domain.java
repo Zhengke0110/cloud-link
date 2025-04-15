da575180b73e6fc5dab169ab049e87010236b688
zhengke <zhengke996@163.com>
2025-04-15 16:40:37 +0800

    feat(domain): 添加域名绑定功能- 新增 domain 表及相关配置 - 实现域名绑定的持久化层 - 添加分片策略配置，支持数据分片 - 优化主键生成器配置，增加默认配置

---
+++ cloud-link-service/src/main/java/fun/timu/cloud/net/link/model/DO/Domain.java
@@ -0,0 +1,56 @@
+package fun.timu.cloud.net.link.model.DO;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName domain
+ */
+@TableName(value ="domain")
+@Data
+public class Domain implements Serializable {
+    /**
+     * 
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 用户自己绑定的域名
+     */
+    private Long accountNo;
+
+    /**
+     * 域名类型，自建custom, 官方offical
+     */
+    private String domainType;
+
+    /**
+     * 
+     */
+    private String value;
+
+    /**
+     * 0是默认，1是禁用
+     */
+    private Integer del;
+
+    /**
+     * 
+     */
+    private Date gmtCreate;
+
+    /**
+     * 
+     */
+    private Date gmtModified;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
