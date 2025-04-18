d0774da6b307e2bb6789e057e75d336875320119
zhengke <zhengke996@163.com>
2025-04-18 11:29:37 +0800

    feat: 添加产品订单管理接口及实现类
    - 新增 ProductOrderManager 接口，用于管理产品订单相关操作
    - 实现 ProductOrderManagerImpl 类，提供产品订单管理的具体实现

---
+++ cloud-shop/src/main/java/fun/timu/cloud/net/shop/manager/impl/ProductOrderManagerImpl.java
@@ -0,0 +1,9 @@
+package fun.timu.cloud.net.shop.manager.impl;
+
+
+import fun.timu.cloud.net.shop.manager.ProductOrderManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductOrderManagerImpl implements ProductOrderManager {
+}
