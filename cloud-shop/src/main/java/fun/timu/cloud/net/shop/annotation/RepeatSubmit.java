3248f32f2d9234970eee7a8f5e530b69cec115d7
zhengke <zhengke996@163.com>
2025-04-18 22:41:05 +0800

    feat(shop): 添加防重提交功能
    - 在 application.yaml 中添加 Redis 配置
    - 新增 MybatisPlus 分页配置
    - 添加 Redisson 分布式锁配置
    - 新增防重提交注解和切面
    - 定义提交订单令牌的缓存 key

---
+++ cloud-shop/src/main/java/fun/timu/cloud/net/shop/annotation/RepeatSubmit.java
@@ -0,0 +1,31 @@
+package fun.timu.cloud.net.shop.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RepeatSubmit {
+
+
+    /**
+     * 防重提交，支持两种，一个是方法参数，一个是令牌
+     */
+    enum Type {PARAM, TOKEN}
+
+    /**
+     * 默认防重提交，是方法参数
+     *
+     * @return
+     */
+    Type limitType() default Type.PARAM;
+
+
+    /**
+     * 加锁过期时间，默认是5秒
+     *
+     * @return
+     */
+    long lockTime() default 5;
+
+}
